25f0da6451e5d5e775080cfb804d4f8f557ac569
Arpan Sen <warpan2020@gmail.com>
2021-07-29 12:38:42 +0530

    Updates

---
+++ precache-manifest.cfcb575ec823fad8258d18986c0766d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b535dac53ba6f4f20490ac463ff887",
+    "url": "/mern-stack-client/index.html"
+  },
+  {
+    "revision": "dc25692a6e1a5a46bd1a",
+    "url": "/mern-stack-client/static/css/main.56854ba0.chunk.css"
+  },
+  {
+    "revision": "221a82726e591e203c1e",
+    "url": "/mern-stack-client/static/js/2.1ebae321.chunk.js"
+  },
+  {
+    "revision": "b52534a5ffbcfa5f4ac7a4883223c24a",
+    "url": "/mern-stack-client/static/js/2.1ebae321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc25692a6e1a5a46bd1a",
+    "url": "/mern-stack-client/static/js/main.6d5fb63e.chunk.js"
+  },
+  {
+    "revision": "7ab5da4ae22f992b105d",
+    "url": "/mern-stack-client/static/js/runtime-main.e5822420.js"
+  }
+]);
